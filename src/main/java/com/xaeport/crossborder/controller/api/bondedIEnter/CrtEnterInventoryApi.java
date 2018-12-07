527c93385582efacba80807f4fc634282b8502e1
李钊阳 <lizhaoyang@xaeport.com>
2018-12-07 17:43:16 +0800

    1.修改及调整账册备案管理，保税进口入区，保税进口出区模块页面显示；

---
+++ src/main/java/com/xaeport/crossborder/controller/api/bondedIEnter/CrtEnterInventoryApi.java
@@ -0,0 +1,151 @@
+//package com.xaeport.crossborder.controller.api.bondedIEnter;
+//
+//import com.alibaba.druid.support.logging.Log;
+//import com.alibaba.druid.support.logging.LogFactory;
+//import com.xaeport.crossborder.configuration.AppConfiguration;
+//import com.xaeport.crossborder.configuration.SystemConstants;
+//import com.xaeport.crossborder.controller.api.BaseApi;
+//import com.xaeport.crossborder.data.ResponseData;
+//import com.xaeport.crossborder.data.entity.Users;
+//import com.xaeport.crossborder.excel.data.ExcelData;
+//import com.xaeport.crossborder.excel.data.ExcelDataInstance;
+//import com.xaeport.crossborder.excel.read.ReadExcel;
+//import com.xaeport.crossborder.service.ordermanage.OrderImportService;
+//import com.xaeport.crossborder.tools.DownloadUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.util.CollectionUtils;
+//import org.springframework.util.StringUtils;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.io.File;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.List;
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/import")
+//public class CrtEnterInventoryApi extends BaseApi {
+//    private Log log = LogFactory.getLog(this.getClass());
+//    @Autowired
+//    AppConfiguration appConfiguration;
+//    @Autowired
+//    OrderImportService orderImportService;
+//
+//    /**
+//     * 新快件上传
+//     *
+//     * @param importTime //进口时间
+//     * @param file       // 上传的文件
+//     */
+//    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+//    public ResponseData MultipartFile(
+//            @RequestParam(value = "importTime", required = false) String importTime,//申报时间
+//            @RequestParam(value = "file", required = false) MultipartFile file,//出口国际邮件模板,
+//            @RequestParam(value = "billNo",required = false) String billNo,
+//            HttpServletRequest request
+//    ) {
+//        HttpSession httpSession = request.getSession();
+//       // if (importTime.isEmpty()) return new ResponseData("进口时间不能为空");
+//        if (file == null) return new ResponseData("请选择要导入的文件");
+//        if (billNo == null) return new ResponseData("提运单号不能为空");
+//
+//        String fileName = file.getOriginalFilename();
+//        if (!fileName.endsWith("xls") && !fileName.endsWith("xlsx")) return new ResponseData("导入文件不为excel文件，请重新选择");
+//
+//        if (file.getSize() > (5 * 1024 * 1024)) return new ResponseData("文件大小超过5M，请重新选择文件");
+//        if (billNo.length()>37) return new ResponseData("提运单号长度超过限制,请重新输入");
+//        //获取企业信息
+//        Users user = this.getCurrentUsers();
+//
+//        int curValue = this.getHashingValue(String.format("%s%s", importTime, user.getId()));
+//        if (this.isRepeatSubmit(httpSession.getAttribute("importTime"), curValue)) return new ResponseData("不允许重复提交");
+//        httpSession.setAttribute("importTime", this.getHashingValue(String.format("%s%s", importTime, user.getId())));
+//
+//        InputStream inputStream;
+//        ReadExcel readExcel = new ReadExcel();
+//        Map<String, Object> map;
+//        int flag;
+//        long startTime = System.currentTimeMillis();
+//        try {
+//            inputStream = file.getInputStream();
+//            String type = "order";
+//            map = readExcel.readExcelData(inputStream, fileName, type);//读取excel数据
+//            if (CollectionUtils.isEmpty(map)) return new ResponseData(String.format("导入<%s>为空", fileName));//获取excel为空
+//            if (map.containsKey("error")) {
+//                httpSession.removeAttribute("importTime");
+//                return new ResponseData(map.get("error"));//返回excel校验的错误信息
+//            } else {
+//                List<List<String>> excelDataList = (List<List<String>>) map.get("excelData");//返回excel的正确数据
+//                if (excelDataList.size() <= 1) return new ResponseData(String.format("导入<%s>中数据为空", fileName));
+//
+//                Map<String, Object> excelMap;
+//                ExcelData excelData = ExcelDataInstance.getExcelDataObject(type);
+//                excelMap = excelData.getExcelData(excelDataList);
+//
+//                String orderNoCount = this.orderImportService.getOrderNoCount(excelMap);
+//                if (!orderNoCount.equals("0")) {
+//                    httpSession.removeAttribute("importTime");
+//                    return new ResponseData("订单号【" + orderNoCount + "】不能重复");
+//                }
+//
+//
+//                flag = this.orderImportService.createOrderForm(excelMap, importTime, user,billNo);//数据创建对应的数据
+//                if (flag == 0) {
+//                    this.log.info("入库耗时" + (System.currentTimeMillis() - startTime));
+//                    httpSession.removeAttribute("importTime");
+//                    return new ResponseData(String.format("跨境电子商务进口订单导入成功！"));
+//                } else {
+//                    httpSession.removeAttribute("importTime");
+//                    return new ResponseData("入库失败");
+//                }
+//            }
+//        } catch (IOException e) {
+//            this.log.error(String.format("导入文件模板错误，文件名:%s", fileName), e);
+//            httpSession.removeAttribute("importTime");
+//            return new ResponseData("导入文件模板错误");
+//        } catch (Exception r) {
+//            this.log.error(String.format("导入文件失败，文件名:%s", fileName), r);
+//            httpSession.removeAttribute("importTime");
+//            return new ResponseData("入库失败");
+//        }
+//
+//    }
+//
+//    /**
+//     * excel 跨境电子商务进口订单模板下载
+//     */
+//    @RequestMapping(value = "/downloadFile")
+//    public void excelModelDownload(HttpServletResponse response,
+//                                   @RequestParam(value = "type") String type) {
+//        Map<String, String> map = appConfiguration.getModelFolder();
+//        String filePath = map.get(type);
+//        File file = new File(filePath);
+//        DownloadUtils.download(response, file, SystemConstants.HTTP_CONTENT_TYPE_EXCEL);
+//    }
+//
+//    private int getHashingValue(String str) {
+//        return str.hashCode();
+//    }
+//
+//    //用于判断同一批订单号、进口时间，企业在导入时发生重复
+//    private boolean isRepeatSubmit(Object preValue, int curValue) {
+//        boolean flag;
+//        if (StringUtils.isEmpty(preValue)) {
+//            return false;
+//        }
+//        String str = preValue.toString();
+//        int value = Integer.parseInt(str);
+//        flag = value == curValue;
+//        return flag;
+//    }
+//
+//
+//}
---
+++ src/main/java/com/xaeport/crossborder/controller/api/bondedIExit/ExitInventoryApi.java
@@ -0,0 +1,150 @@
+//package com.xaeport.crossborder.controller.api.bondedIExit;
+//
+//import com.alibaba.druid.support.logging.Log;
+//import com.alibaba.druid.support.logging.LogFactory;
+//import com.xaeport.crossborder.configuration.AppConfiguration;
+//import com.xaeport.crossborder.configuration.SystemConstants;
+//import com.xaeport.crossborder.controller.api.BaseApi;
+//import com.xaeport.crossborder.data.ResponseData;
+//import com.xaeport.crossborder.data.entity.DataList;
+//import com.xaeport.crossborder.data.entity.OrderSum;
+//import com.xaeport.crossborder.data.entity.Users;
+//import com.xaeport.crossborder.data.status.StatusCode;
+//import com.xaeport.crossborder.service.ordermanage.OrderDeclareSevice;
+//import com.xaeport.crossborder.tools.DownloadUtils;
+//import com.xaeport.crossborder.tools.GetIpAddr;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.util.StringUtils;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.File;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//
+///*
+// * 订单申报
+// */
+//@RestController
+//@RequestMapping("/api/orderManage")
+//public class ExitInventoryApi extends BaseApi {
+//
+//    private Log logger = LogFactory.getLog(this.getClass());
+//
+//    @Autowired
+//    AppConfiguration appConfiguration;
+//    @Autowired
+//    OrderDeclareSevice orderDeclareService;
+//
+//    /*
+//     * 邮件申报查询
+//     */
+//    @RequestMapping("/queryOrderDeclare")
+//    public ResponseData queryOrderDeclare(
+//            //身份验证
+//            @RequestParam(required = false) String startFlightTimes,
+//            @RequestParam(required = false) String endFlightTimes,
+//            @RequestParam(required = false) String billNo,
+//            @RequestParam(required = false) String dataStatus
+//    ) {
+//        this.logger.debug(String.format("查询邮件申报条件参数:[startFlightTimes:%s,endFlightTimes:%s,billNo:%s,dataStatus:%s]", startFlightTimes, endFlightTimes, billNo, dataStatus));
+//        Map<String, String> paramMap = new HashMap<String, String>();
+//        //查询参数
+//        paramMap.put("startFlightTimes", StringUtils.isEmpty(startFlightTimes) ? null : startFlightTimes);
+//        paramMap.put("endFlightTimes", StringUtils.isEmpty(endFlightTimes) ? null : endFlightTimes);
+//
+//        paramMap.put("billNo", billNo);
+//        paramMap.put("dataStatus", dataStatus);
+//
+//        paramMap.put("entId", this.getCurrentUserEntId());
+//        paramMap.put("roleId", this.getCurrentUserRoleId());
+//
+//        DataList<OrderSum> dataList = new DataList<>();
+//        List<OrderSum> resultList = new ArrayList<OrderSum>();
+//        try {
+//            //查询列表
+//            resultList = orderDeclareService.queryOrderDeclareList(paramMap);
+//        } catch (Exception e) {
+//            this.logger.error("查询订单申报数据失败", e);
+//            return new ResponseData("获取订单申报数据错误", HttpStatus.BAD_REQUEST);
+//        }
+//        return new ResponseData(resultList);
+//    }
+//
+//
+//    /**
+//     * 订单单申报-提交海关
+//     **/
+//    @RequestMapping(value = "/submitCustom", method = RequestMethod.POST)
+//    public ResponseData saveSubmitCustom(
+//            @RequestParam(required = false) String submitKeys,
+//            HttpServletRequest request) {
+//        this.logger.info("订单申报客户端操作地址为 " + GetIpAddr.getRemoteIpAdd(request));
+//        if (StringUtils.isEmpty(submitKeys)) {
+//            return rtnResponse("false", "请先勾选要提交海关的订单信息！");
+//        }
+//        Users currentUser = this.getCurrentUsers();
+//        Map<String, String> paramMap = new HashMap<>();
+//        paramMap.put("opStatus", StatusCode.DDSBZ);//订单申报中
+//        paramMap.put("opStatusWhere", StatusCode.DDDSB);//订单待申报
+//        paramMap.put("currentUserId", currentUser.getId());
+//        paramMap.put("submitKeys", submitKeys);//提运单号
+//
+//        // 调用订单申报Service 获取提交海关结果
+//        boolean flag = orderDeclareService.updateSubmitCustom(paramMap);
+//        if (flag) {
+//            return rtnResponse("true", "订单申报海关提交成功！");
+//        } else {
+//            return rtnResponse("false", "订单申报海关提交失败！");
+//        }
+//    }
+//
+//    /**
+//     * 订单报文下载
+//     **/
+//    @RequestMapping(value = "/orderXmlDownload", method = RequestMethod.POST)
+//    public ResponseData orderXmlDownload(
+//            @RequestParam(required = false) String submitKeys,
+//            HttpServletRequest request) {
+//        this.logger.info("订单申报客户端操作地址为 " + GetIpAddr.getRemoteIpAdd(request));
+//        if (StringUtils.isEmpty(submitKeys)) {
+//            return rtnResponse("false", "请先勾选要下载的订单信息！");
+//        }
+//        Users currentUser = this.getCurrentUsers();
+//        Map<String, String> paramMap = new HashMap<>();
+//        paramMap.put("opStatus", StatusCode.DDBWSCZ);//订单报文生成中
+//        paramMap.put("opStatusWhere", StatusCode.DDDSB);//订单待申报
+//        paramMap.put("currentUserId", currentUser.getId());
+//
+//        paramMap.put("submitKeys", submitKeys);//提运单号
+//
+//        boolean flag = orderDeclareService.orderXmlDownload(paramMap);
+//        String orderZipPath = orderDeclareService.OrderXml(this.getCurrentUserEntId());
+//        if (!StringUtils.isEmpty(orderZipPath)) {
+//            return rtnResponse("1" + orderZipPath, "订单报文生成提交成功");
+//        } else {
+//            return rtnResponse("0" + orderZipPath, "订单报文生成提交失败");
+//        }
+//    }
+//
+//    /**
+//     * excel 跨境电子商务进口订单模板下载
+//     */
+//    @RequestMapping(value = "/downloadFile")
+//    public void excelModelDownload(
+//            HttpServletResponse response,
+//            @RequestParam(value = "type") String type
+//    ) {
+//        File file = new File(type);
+//        DownloadUtils.download(response, file, SystemConstants.HTTP_CONTENT_TYPE_ZIP);
+//    }
+//
+//}
