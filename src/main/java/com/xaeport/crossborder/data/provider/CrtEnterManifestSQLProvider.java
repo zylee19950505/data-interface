1a299d420e0f7798e1f0f78219923a693461ec51
mamba <yantonghui@xaeport.com>
2018-12-26 15:44:33 +0800

    1. 入区核放单暂存

--- src/main/java/com/xaeport/crossborder/data/provider/CrtEnterManifestSQLProvider.java
+++ src/main/java/com/xaeport/crossborder/data/provider/CrtEnterManifestSQLProvider.java
@@ -1,4 +1,17 @@
 package com.xaeport.crossborder.data.provider;
 
+import org.apache.ibatis.jdbc.SQL;
+
+import java.util.Map;
+
 public class CrtEnterManifestSQLProvider extends BaseSQLProvider{
+
+
+    public String queryCrtEnterManifest(Map<String, String> paramMap){
+        return new SQL(){
+            {
+
+            }
+        }.toString();
+    }
 }
